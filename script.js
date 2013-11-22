9d4c0a52e7804bdd6684dfa4a6e70b65f43f2206
Ryan Waters <ryananthonywaters@gmail.com>
2013-11-21 16:40:21 -0800

    some styling and refactoring, everything is working

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
+// load the window
 window.onload = function() {
 
+  // get user inputs from form
   var searchForm = document.getElementById('search');
   var queryInput = document.getElementById('query');
   var moviesList = document.getElementById('moviesList');
   var resultsDiv = document.getElementById('results');
-  var list;
-
-
+  
+  
   searchForm.onsubmit = function(e) {
     e.preventDefault();
 
-    var query = queryInput.value;
+    // 
+    var query = queryInput.value;  
     var request = new XMLHttpRequest();
     request.open('Get', 'http://omdbapi.com/?s=' + query)
 
@@ -18,14 +20,16 @@ window.onload = function() {
       if (request.readyState === 4 && request.status === 200) {
         movieResults = request.response;
         parsedMovies = JSON.parse(movieResults)
-
+        moviesList.innerHTML = "" // stops from duplicating results on multiple submits
         for (var i = 0; i < parsedMovies.Search.length; i++) {
           var list = document.createElement('li');
-          var movieTitle = document.createTextNode(parsedMovies.Search[i]['Title']);
+          var movieTitle = document.createTextNode(parsedMovies.Search[i]['Title'] + " ");
           var movieYear = document.createTextNode(parsedMovies.Search[i]['Year']);
           list.appendChild(movieTitle);
           list.appendChild(movieYear);
+
           moviesList.appendChild(list);
+
         }
       }
     }
