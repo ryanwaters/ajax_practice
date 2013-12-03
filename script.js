4f4cc41c36c0091631e25c85ded7182bcf0c374c
Ryan Waters <ryananthonywaters@gmail.com>
2013-12-03 13:01:51 -0800

    refactored, added posters, imdb linking, and styling

--- script.js
+++ script.js
@@ -1,40 +1,56 @@
 // load the window
 window.onload = function() {
 
-  // get user inputs from form
-  var searchForm = document.getElementById('search');
-  var queryInput = document.getElementById('query');
-  var moviesList = document.getElementById('moviesList');
-  var resultsDiv = document.getElementById('results');
-  
-  
+  var searchForm      = document.getElementById("search");
+  var queryInput      = document.getElementById("query");
+  var resultsList     = document.getElementById("results");
+  var anchor          = document.getElementById("matrix");
+  var posterContainer = document.getElementById("poster")
+
   searchForm.onsubmit = function(e) {
     e.preventDefault();
 
-    // 
-    var query = queryInput.value;  
     var request = new XMLHttpRequest();
-    request.open('Get', 'http://omdbapi.com/?s=' + query)
-
     request.onreadystatechange = function() {
       if (request.readyState === 4 && request.status === 200) {
-        movieResults = request.response;
-        parsedMovies = JSON.parse(movieResults)
-        moviesList.innerHTML = "" // stops from duplicating results on multiple submits
-        for (var i = 0; i < parsedMovies.Search.length; i++) {
-          var list = document.createElement('li');
-          var movieTitle = document.createTextNode(parsedMovies.Search[i]['Title'] + " ");
-          var movieYear = document.createTextNode(parsedMovies.Search[i]['Year']);
-          var imdbID = document.createTextNode(parsedMovies.Search[i]['imdbID']);
-          list.appendChild(movieTitle);
-          list.appendChild(movieYear);
-
-          moviesList.appendChild(list);
+        var results = JSON.parse(request.response);
+        var movies  = results["Search"];
+        resultsList.innerHTML = "";
+        for (var i = 0; i < movies.length; i++) {
+          var movie = movies[i];
           
-        }
+          var movieListItem = document.createElement("li");
+          var movieAnchor   = document.createElement("a");
 
+          movieAnchor.setAttribute("href", "http://www.imdb.com/title/" + movie["imdbID"])
+          movieAnchor.innerText = movie["Title"] + " (" + movie["Year"] + ")";
+          movieAnchor.addEventListener("click", function(event) {
+            event.preventDefault();
+
+            posterContainer.innerHTML = "";
+
+            var request = new XMLHttpRequest();
+            request.onreadystatechange = function() {
+              if (request.readyState === 4 && request.status === 200) {
+                var movie = JSON.parse(request.response);
+                var poster = document.createElement("img");
+                poster.setAttribute("src", movie["Poster"]);
+                poster.setAttribute("alt", movie["Title"] + " Poster");
+                posterContainer.appendChild(poster);
+              }
+            }
+
+            var imdbId = event.target.href.split("title/")[1];
+            request.open("GET", "http://omdbapi.com/?i=" + imdbId);
+            request.send();
+          });
+
+          movieListItem.appendChild(movieAnchor);
+          resultsList.appendChild(movieListItem);
+        }
       }
     }
-    request.send()
-  }
+    request.open("GET", "http://omdbapi.com/?s=" + queryInput.value);
+    request.send();
+  };
 }
